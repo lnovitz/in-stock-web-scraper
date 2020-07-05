47eab2375ce2de7baaeeaf5010acf0cb1553af65
Lianna <liannanovitz@gmail.com>
2020-07-04 22:20:11 -0700

    create functions

--- InStock.py
+++ InStock.py
@@ -7,28 +7,40 @@
 import requests 
 from lxml import html
 
-# getting the data 
-page = requests.get('https://bananarepublic.gap.com/browse/product.do?pid=548041&bvstate=pg:2/ct:r#pdp-page-content')
-tree = html.fromstring(page.content)
-new = tree.xpath("//html//text()") #get tree
+#define URL
+# https://bananarepublic.gap.com/browse/product.do?pid=548041&bvstate=pg:2/ct:r#pdp-page-content
 
-output = open('banana.txt', 'w') #write html to a text file
-output.write(str(new))
+def grabWebLink():
+    webLink= input('Enter the webpage you want to scrape: ')
+    return webLink
+
+# getting the data 
+def getHTML(webLink):
+    page = requests.get(webLink)
+    tree = html.fromstring(page.content)
+    treeOutput = tree.xpath("//html//text()")
+    return str(treeOutput) #get html output as a string
+
+def writeHTML(treeOutput):
+    output = open('banana.txt', 'w') #write html to a text file
+    output.write(treeOutput)
+    output.close()
+
+# writeHTML(getHTML(grabWebLink())) # this command is only needed once to write the html to a text file
 
 inputFile = open('banana.txt', 'r') #read in the html 
 stockCount = 0
 greater10count = 0
 for x in inputFile:
-    if x.lower().find('instock') != -1:
+    print(type(x))
+    #if x.lower().find('instock') != -1:
         #print(x + '\n\n\n\n--------------------------\n')
-        stockCount += 1
-    elif len(x) > 10:
-        greater10count += 1
-
+    #    stockCount += 1
+    #elif len(x) > 10:
+    #    greater10count += 1
 
 print(stockCount, greater10count)
 
-output.close()
 inputFile.close()
 
     
